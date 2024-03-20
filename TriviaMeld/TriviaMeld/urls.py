2931e91d5953f97f3abe62c259ca900a4f5ffcc5
millywanderi <wanderinyambura5@gmail.com>
2024-03-20 23:44:21 +0300

    Mapped the quiz urls to the main application

--- TriviaMeld/TriviaMeld/urls.py
+++ TriviaMeld/TriviaMeld/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('homepage/', include('homepage.urls')),
+    path('quiz/', include('quiz.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
