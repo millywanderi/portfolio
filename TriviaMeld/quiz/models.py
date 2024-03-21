0319cd9b4917b9738dfecff72b576d37f9085e34
millywanderi <wanderinyambura5@gmail.com>
2024-03-21 14:37:33 +0300

    Generated the quiz id

--- TriviaMeld/quiz/models.py
+++ TriviaMeld/quiz/models.py
@@ -1,8 +1,27 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+def generate_id():
+    """"Generates quiz id"""
+    random_uuid = uuid4()
+    quiz_id = f"{random_uuid}"
+    return quiz_id
+
 class Quiz(models.Model):
     """Model for quiz selection"""
     title = models.CharField(max_length=100)
     description = models.TextField()
+    quiz_id = models.CharField(max_length=10, default=generate_id, editable=False)
+
+
+    class Meta:
+        ordering = [
+            'title' 
+        ]
+
+    def __str__(self):
+        return f"({self.pk}) quiz {self.title}"
+    
+
     
