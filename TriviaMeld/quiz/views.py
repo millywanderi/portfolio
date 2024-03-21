8c200c927d809b6d502b5e459207837058a290ff
millywanderi <wanderinyambura5@gmail.com>
2024-03-21 14:40:59 +0300

    Created quiz interface view

--- TriviaMeld/quiz/views.py
+++ TriviaMeld/quiz/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Quiz
 
 # Create your views here.
@@ -11,3 +11,9 @@ def quiz_detail(request, quiz_id):
     """Provides deatails about the quiz"""
     quiz = Quiz.objects.get(pk=quiz_id)
     return render(request, 'quiz/quiz_detail.html', {'quiz': quiz})
+
+def quiz_interface(request, quiz_id):
+    """Retrieve the quiz object from the database based on quiz_id"""
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    # questions = quiz.question_set.all()
+    return render(request, 'quiz/quiz_interface.html', {'quiz': quiz})
