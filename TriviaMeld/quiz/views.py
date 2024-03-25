a79ea64a6354526a653724721d5ba64076dc8715
millywanderi <wanderinyambura5@gmail.com>
2024-03-25 23:40:34 +0300

    Created submit answer url

--- TriviaMeld/quiz/views.py
+++ TriviaMeld/quiz/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Quiz
+from .models import Quiz, Question, Option
 
 # Create your views here.
 def quiz_selection(request):
@@ -7,13 +7,45 @@ def quiz_selection(request):
     quizzes = Quiz.objects.all()
     return render(request, 'quiz/quiz_selection.html', {'quizzes': quizzes})
 
-def quiz_detail(request, quiz_id):
-    """Provides deatails about the quiz"""
-    quiz = Quiz.objects.get(pk=quiz_id)
-    return render(request, 'quiz/quiz_detail.html', {'quiz': quiz})
 
 def quiz_interface(request, quiz_id):
     """Retrieve the quiz object from the database based on quiz_id"""
-    quiz = get_object_or_404(Quiz, pk=quiz_id)
-    # questions = quiz.question_set.all()
-    return render(request, 'quiz/quiz_interface.html', {'quiz': quiz})
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    # questions = quiz.questions.all()
+    questions = Question.objects.filter(quiz=quiz)
+    context = {
+        'quiz': quiz,
+        'questions': questions,
+    }
+    return render(request, 'quiz/quiz_interface.html', context)
+
+def submit_answer(request, quiz_id):
+    """Submitting the answer"""
+    quiz = Quiz.objects.get(id=quiz_id)
+    if request.method == 'POST':
+        print(request.method)
+        question1_answer = request.POST['Question 1']
+        question2_answer = request.POST['Question 2']
+        question3_answer = request.POST['Question 3']
+        answers_list = [
+            question1_answer,
+            question2_answer,
+            question3_answer
+        ]
+
+        questions = Question.objects.filter(quiz=quiz)
+        score = 0
+        total_score = 0
+
+        for question in questions:
+            correct_answer = Option.objects.get(question=question, is_correct=True)
+            for answer in answers_list:
+                print("correct_answer", correct_answer.text, type(correct_answer.text))
+                print("user_answer", answer, type(answer))
+                if correct_answer.text == answer:
+                    score += 1           
+        total_score = f"{score}/3"
+        context = {
+            'total_score': total_score,
+        }
+        return render(request, 'quiz/quiz_feedback.html', context)
