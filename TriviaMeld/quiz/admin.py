c1f1ba3afda9b353c7af44b6683897da7ee8fc1c
millywanderi <wanderinyambura5@gmail.com>
2024-03-25 23:37:22 +0300

    Created QuizAdmin class to layout the quizzes

--- TriviaMeld/quiz/admin.py
+++ TriviaMeld/quiz/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Quiz
+from .models import Quiz, Question, Option
 
 # Register your models here.
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "description", "quiz_id")
 
-admin.site.register(Quiz)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question)
+admin.site.register(Option)
